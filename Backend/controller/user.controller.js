cca3ee0357bb7e8c000e1f64da0649a2cbea1449
Unnati-Gupta24 <guptaunnati031@gmail.com>
2024-11-10 02:20:35 +0530

    backend

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,21 +1,29 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
 
-export const signup = (req,res) => {
+export const signup = async (req,res) => {
     try {
          const { name, email, password, confirmpassword } = req.body;
          if (password !== confirmpassword) {
            return res.status(400).json({ message: "Passwords do not match" });
          }
-         const user = User.findOne({ email });
+         const user = await User.findOne({ email });
          if (user) {
            return res.status(400).json({ messages: "Email already exists" });
          }
-         const newUser = new User({
+
+        //Hashing the password
+         const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Log the hashed password for debugging 
+         console.log("Hashed Password:", hashedPassword);
+
+         const newUser = await new User({
            name,
            email,
-           password,
+           password: hashedPassword,
          });
-         newUser
+         await newUser
            .save()
            .then(() => 
             res.json({ message: "User registration successful" })
