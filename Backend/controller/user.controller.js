d4761da2b0f27afc0fac80ea745023c226842eed
Unnati-Gupta24 <guptaunnati031@gmail.com>
2024-11-10 13:22:06 +0530

    backend update

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -33,3 +33,35 @@ export const signup = async (req,res) => {
         res.status(500).json({ message: "Server error" });
     }
 }; 
+
+
+export const login = async(req,res)=>{
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({email})
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!user || !isMatch){
+      return res.status(404).json({ message: "invalid User or Password" });
+    }
+    createTokenAndSaveCookie(user._id, res);
+    res.status(201).json({ message: "User logged in successfully", 
+      user:{
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      }, });
+  } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Server error" });
+  }
+}
+
+export const logout = async(req,res)=>{
+  try {
+    res.clearCookie('jwt');
+    res.status(200).json({ message: "User logged out successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+}
