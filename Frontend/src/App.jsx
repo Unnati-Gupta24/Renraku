cc19482331a0e41a99441606f1bcd7eac38530ad
Unnati-Gupta24 <guptaunnati031@gmail.com>
2024-11-10 17:00:22 +0530

    Login Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Left from './home/left/left'
 import Right from './home/right/right'
 import Logout from './home/left1/Logout'
 import Signup from './components/Signup'
+import Login from './components/Login'
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <Left></Left>
       <Right></Right> 
     </div>  */}
-    <Signup/>
+    <Login/>
     </>
   )
 }
