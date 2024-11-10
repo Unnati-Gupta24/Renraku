ea2166cc41d3cc3963f0e6faa7cee989345ffbc5
Unnati-Gupta24 <guptaunnati031@gmail.com>
2024-11-10 15:44:51 +0530

    Add Signup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import Left from './home/left/left'
 import Right from './home/right/right'
 import Logout from './home/left1/Logout'
+import Signup from './components/Signup'
 
 function App() {
   return (
     <>
-    <div className="flex h-screen">
+    {/*  <div className="flex h-screen">
       <Logout></Logout>
       <Left></Left>
       <Right></Right> 
-    </div>
+    </div>  */}
+    <Signup/>
     </>
   )
 }
